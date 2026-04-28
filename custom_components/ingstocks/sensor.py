2124471081fde8bab889cc79dc13016cad9523cc
marq24 <marquardt24@gmail.com>
2026-04-28 11:45:54 +0200

    implemented 'CoordinatorEntity' (update states max every second)

--- custom_components/ingstocks/sensor.py
+++ custom_components/ingstocks/sensor.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 import logging
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
 from homeassistant.components.sensor import (
     SensorEntity,
     SensorDeviceClass,
     SensorStateClass,
 )
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import dt as dt_util
@@ -166,6 +166,7 @@ class INGStockBaseSensor(CoordinatorEntity, SensorEntity):
 
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(self.coordinator.async_add_listener(self.async_write_ha_state))
+        await super().async_added_to_hass()
 
 
 class INGStockValueSensor(INGStockBaseSensor):
